b7e5f3c62148d8b708cf3d6905735e678dcd3b79
tomar-143 <manishdd_cse22@its.edu.in>
2025-06-18 23:25:53 +0530

    Delete part9 directory

--- part9/appC.js
+++
@@ -1,3 +0,0 @@
-const mathOperations = require("./mathOperationsC.js");
- 
-console.log(mathOperations.add(2,2));
--- part9/appM.js
+++
@@ -1,8 +0,0 @@
-//default import
-import multiply from "./mathOperationsM.js";
-
-//named import
-import {add, subtract } from "./mathOperationM.js";
-
-console.log(multiply(2, 2));
-console.log(2, 2);
